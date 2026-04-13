02a1731b2380a1553c326b1a2d815aec3d8b5f01
CataAguirre2009 <amaguirremizzau@escuelasproa.edu.ar>
2026-04-13 14:29:57 -0300

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['gender', 'math score']])
+
+print(df.head(8))
+
+print(df.tail(4))
+
+print(df.dtypes)
+
+print(df.shape)
+
+print(df.count())
